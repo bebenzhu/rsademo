4007724cb6339548d45badfaca949e1c0bc6d3bb
hchen10 <hchen10@amaroft.com>
2020-01-20 16:11:19 +0800

    加密实现

---
+++ src/main/java/com/example/rsademo/RSAUtils.java
@@ -0,0 +1,112 @@
+package com.example.rsademo;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+
+import javax.crypto.Cipher;
+import java.security.*;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RSAUtils {
+
+    public static final String CHARSET = "UTF-8";
+    public static final String RSA_ALGORITHM = "RSA"; // ALGORITHM ['ælgərɪð(ə)m] 算法的意思
+
+    public static Map<String, String> createKeys(int keySize) {
+        // 为RSA算法创建一个KeyPairGenerator对象
+        KeyPairGenerator kpg;
+        try {
+            kpg = KeyPairGenerator.getInstance(RSA_ALGORITHM);
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalArgumentException("No such algorithm-->[RSA]");
+        }
+
+        // 初始化KeyPairGenerator对象,密钥长度
+        kpg.initialize(keySize);
+        // 生成密匙对
+        KeyPair keyPair = kpg.generateKeyPair();
+        // 得到公钥
+        Key publicKey = keyPair.getPublic();
+//        System.out.println(publicKey);
+        String publicKeyStr = new String(Base64.encodeBase64(publicKey.getEncoded()));
+        // 得到私钥
+        Key privateKey = keyPair.getPrivate();
+//        System.out.println(privateKey);
+        String privateKeyStr = new String(Base64.encodeBase64(privateKey.getEncoded()));
+        // map装载公钥和私钥
+        Map<String, String> keyPairMap = new HashMap<String, String>();
+        keyPairMap.put("publicKey", publicKeyStr);
+        keyPairMap.put("privateKey", privateKeyStr);
+        // 返回map
+        return keyPairMap;
+    }
+
+    /**
+     * 得到公钥
+     * @param publicKey  密钥字符串（经过base64编码）
+     * @throws Exception
+     */
+    public static RSAPublicKey getPublicKey(String publicKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        // 通过X509编码的Key指令获得公钥对象
+        KeyFactory keyFactory = KeyFactory.getInstance(RSA_ALGORITHM);
+        X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(Base64.decodeBase64(publicKey));
+        RSAPublicKey key = (RSAPublicKey) keyFactory.generatePublic(x509KeySpec);
+        return key;
+    }
+
+    /**
+     * 得到私钥
+     * @param privateKey  密钥字符串（经过base64编码）
+     * @throws Exception
+     */
+    public static RSAPrivateKey getPrivateKey(String privateKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        // 通过PKCS#8编码的Key指令获得私钥对象
+        KeyFactory keyFactory = KeyFactory.getInstance(RSA_ALGORITHM);
+        PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(Base64.decodeBase64(privateKey));
+        RSAPrivateKey key = (RSAPrivateKey) keyFactory.generatePrivate(pkcs8KeySpec);
+        return key;
+    }
+
+    /**
+     * 公钥加密
+     * @param data
+     * @param publicKey
+     * @return
+     */
+    public static String publicEncrypt(String data, RSAPublicKey publicKey) {
+        try {
+            Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            return Base64.encodeBase64String(cipher.doFinal(data.getBytes(CHARSET)));
+        } catch (Exception e) {
+            throw new RuntimeException("加密字符串[" + data + "]时遇到异常", e);
+        }
+    }
+
+    /**
+     * 私钥解密
+     * @param data
+     * @param privateKey
+     * @return
+     */
+
+    public static String privateDecrypt(String data, RSAPrivateKey privateKey) {
+        try {
+            Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, privateKey);
+            return new String(cipher.doFinal(Base64.decodeBase64(data.getBytes(CHARSET))));
+        } catch (Exception e) {
+            throw new RuntimeException("解密字符串[" + data + "]时遇到异常", e);
+        }
+    }
+
+
+
+
+}
--- src/test/java/com/example/rsademo/RsademoApplicationTests.java
+++ src/test/java/com/example/rsademo/RsademoApplicationTests.java
@@ -3,11 +3,30 @@ package com.example.rsademo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Map;
+
 @SpringBootTest
 class RsademoApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws InvalidKeySpecException, NoSuchAlgorithmException {
+        String data = "高鹏辉是傻叉！！！";
+
+        Map<String, String> keyPairMap = RSAUtils.createKeys(1024);
+        //公钥
+        String publicKeyStr = keyPairMap.get("publicKey");
+        System.out.println("公钥:"+publicKeyStr);
+        //私钥
+        String privateKeyStr = keyPairMap.get("privateKey");
+        System.out.println("私钥:"+privateKeyStr);
+
+        String encryptData = RSAUtils.publicEncrypt(data, RSAUtils.getPublicKey(publicKeyStr));
+        System.out.println("加密后数据:"+encryptData);
+
+        String decryptData = RSAUtils.privateDecrypt(encryptData, RSAUtils.getPrivateKey(privateKeyStr));
+        System.out.println("解密后数据:"+decryptData);
     }
 
 }
